54a8a1fe5f9085c1eca6fbeacb7bc7b578f71707
calebbabin <object.caleb@gmail.com>
2019-11-21 11:37:43 -0600

    Sorts by featured posts first

--- src/templates/index.js
+++ src/templates/index.js
@@ -63,7 +63,7 @@ export default Index
 export const pageQuery = graphql`
     query GhostPostQuery($limit: Int!, $skip: Int!) {
         allGhostPost(
-            sort: { order: DESC, fields: [published_at] },
+            sort: { order: DESC, fields: [featured, published_at] },
             limit: $limit,
             skip: $skip,
         ) {
