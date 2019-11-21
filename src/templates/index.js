afc3b80984be7ba9a3a3411f8fce6592370e7852
calebbabin <object.caleb@gmail.com>
2019-11-21 16:39:23 -0600

    remove "sort by featured" from blog index

--- src/templates/index.js
+++ src/templates/index.js
@@ -153,7 +153,7 @@ export default Index
 export const pageQuery = graphql`
     query GhostPostQuery($limit: Int!, $skip: Int!) {
         allGhostPost(
-            sort: { order: DESC, fields: [featured, published_at] },
+            sort: { order: DESC, fields: [published_at] },
             limit: $limit,
             skip: $skip,
         ) {
