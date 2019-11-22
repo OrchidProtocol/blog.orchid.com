4099c4a9aa8472aa45c6ae8a469f54b93f9c79b5
calebbabin <object.caleb@gmail.com>
2019-11-21 18:43:49 -0600

    fix +1 bug

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,7 +7,7 @@ function currentYear () {
 function getCustomFormatedDate (timestamp) {
     const date = new Date(timestamp);
     const month = months[date.getMonth()],
-        day = date.getDate()+1,
+        day = date.getDate(),
         year = date.getFullYear();
 
         return `${month} ${day}, ${year}`;
