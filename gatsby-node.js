619e4ae44e7f2cc94f77b90299bbb9beb8a8be3e
calebbabin <object.caleb@gmail.com>
2019-12-03 17:28:51 -0600

    start integrating netlify cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 const path = require(`path`)
 const { postsPerPage } = require(`./src/utils/siteConfig`)
 const { paginate } = require(`gatsby-awesome-pagination`)
+const { createFilePath } = require('gatsby-source-filesystem')
+const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 /**
  * Here is the place where Gatsby creates the URLs for all the
@@ -8,150 +10,130 @@ const { paginate } = require(`gatsby-awesome-pagination`)
  */
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
+	const { createPage } = actions
 
-    const result = await graphql(`
-        {
-            allGhostPost(sort: { order: ASC, fields: published_at }) {
-                edges {
-                    node {
-                        slug
-                    }
-                }
-            }
-            allGhostTag(sort: { order: ASC, fields: name }) {
-                edges {
-                    node {
-                        slug
-                        url
-                        postCount
-                    }
-                }
-            }
-            allGhostPage(sort: { order: ASC, fields: published_at }) {
-                edges {
-                    node {
-                        slug
-                        url
-                    }
-                }
-            }
-            allGhostPage(sort: { order: ASC, fields: published_at }) {
-                edges {
-                    node {
-                        slug
-                        url
-                    }
-                }
-            }
-        }
+	const result = await graphql(`
+		{
+			allMarkdownRemark(limit: 1000) {
+				edges {
+					node {
+						id
+						fields {
+							slug
+						}
+						frontmatter {
+							tags
+							templateKey
+							public
+						}
+					}
+				}
+			}
+		}
     `)
 
-    // Check for any errors
-    if (result.errors) {
-        throw new Error(result.errors)
-    }
+	// Check for any errors
+	if (result.errors) {
+		throw new Error(result.errors)
+	}
 
-    // Extract query results
-    const tags = result.data.allGhostTag.edges
-    const pages = result.data.allGhostPage.edges
-    const posts = result.data.allGhostPost.edges
+	// Extract query results
+	const posts = result.data.allMarkdownRemark.edges
 
-    // Load templates
-    const indexTemplate = path.resolve(`./src/templates/index.js`)
-    const tagsTemplate = path.resolve(`./src/templates/tag.js`)
-    const pageTemplate = path.resolve(`./src/templates/page.js`)
-    const postTemplate = path.resolve(`./src/templates/post.js`)
+	// Extract tags from posts
+	const tags = [];
+	const tagsObj = {};
+	for (let index = 0; index < posts.length; index++) {
+		const edge = posts[index];
+		for (let index = 0; index < edge.node.frontmatter.tags.length; index++) {
+			const tag = edge.node.frontmatter.tags[index];
+			tagsObj[tag] = true;
+		}
+	}
+	for (const key in tags) {
+		if (tags.hasOwnProperty(key)) {
+			const element = tags[key];
+			tags.push(key);
+		}
+	}
 
-    // Create tag pages
-    tags.forEach(({ node }) => {
-        const totalPosts = node.postCount !== null ? node.postCount : 0
-        const numberOfPages = Math.ceil(totalPosts / postsPerPage)
+	// Load templates
+	const indexTemplate = path.resolve(`./src/templates/index.js`)
+	const tagsTemplate = path.resolve(`./src/templates/tag.js`)
+	const postTemplate = path.resolve(`./src/templates/post.js`)
 
-        // This part here defines, that our tag pages will use
-        // a `/tag/:slug/` permalink.
-        node.url = `/tag/${node.slug}/`
+	// Create tag pages
+	tags.forEach(({ node }) => {
+		const totalPosts = node.postCount !== null ? node.postCount : 0
+		const numberOfPages = Math.ceil(totalPosts / postsPerPage)
 
-        Array.from({ length: numberOfPages }).forEach((_, i) => {
-            const currentPage = i + 1
-            const prevPageNumber = currentPage <= 1 ? null : currentPage - 1
-            const nextPageNumber =
-                currentPage + 1 > numberOfPages ? null : currentPage + 1
-            const previousPagePath = prevPageNumber
-                ? prevPageNumber === 1
-                    ? node.url
-                    : `${node.url}page/${prevPageNumber}/`
-                : null
-            const nextPagePath = nextPageNumber
-                ? `${node.url}page/${nextPageNumber}/`
-                : null
+		// This part here defines, that our tag pages will use
+		// a `/tag/:slug/` permalink.
+		node.url = `/tag/${node.slug}/`
 
-            createPage({
-                path: i === 0 ? node.url : `${node.url}page/${i + 1}/`,
-                component: tagsTemplate,
-                context: {
-                    // Data passed to context is available
-                    // in page queries as GraphQL variables.
-                    slug: node.slug,
-                    limit: postsPerPage,
-                    skip: i * postsPerPage,
-                    numberOfPages: numberOfPages,
-                    humanPageNumber: currentPage,
-                    prevPageNumber: prevPageNumber,
-                    nextPageNumber: nextPageNumber,
-                    previousPagePath: previousPagePath,
-                    nextPagePath: nextPagePath,
-                },
-            })
-        })
-    })
+		Array.from({ length: numberOfPages }).forEach((_, i) => {
+			const currentPage = i + 1
+			const prevPageNumber = currentPage <= 1 ? null : currentPage - 1
+			const nextPageNumber =
+				currentPage + 1 > numberOfPages ? null : currentPage + 1
+			const previousPagePath = prevPageNumber
+				? prevPageNumber === 1
+					? node.url
+					: `${node.url}page/${prevPageNumber}/`
+				: null
+			const nextPagePath = nextPageNumber
+				? `${node.url}page/${nextPageNumber}/`
+				: null
 
-    // Create pages
-    pages.forEach(({ node }) => {
-        // This part here defines, that our pages will use
-        // a `/:slug/` permalink.
-        node.url = `/${node.slug}/`
+			createPage({
+				path: i === 0 ? node.url : `${node.url}page/${i + 1}/`,
+				component: tagsTemplate,
+				context: {
+					// Data passed to context is available
+					// in page queries as GraphQL variables.
+					slug: node.slug,
+					limit: postsPerPage,
+					skip: i * postsPerPage,
+					numberOfPages: numberOfPages,
+					humanPageNumber: currentPage,
+					prevPageNumber: prevPageNumber,
+					nextPageNumber: nextPageNumber,
+					previousPagePath: previousPagePath,
+					nextPagePath: nextPagePath,
+				},
+			})
+		})
+	})
 
-        createPage({
-            path: node.url,
-            component: pageTemplate,
-            context: {
-                // Data passed to context is available
-                // in page queries as GraphQL variables.
-                slug: node.slug,
-            },
-        })
-    })
+	// Create post pages
+	posts.forEach(({ node }) => {
+		// This part here defines, that our posts will use
+		// a `/:slug/` permalink.
+		node.url = `/${node.slug}/`
 
-    // Create post pages
-    posts.forEach(({ node }) => {
-        // This part here defines, that our posts will use
-        // a `/:slug/` permalink.
-        node.url = `/${node.slug}/`
+		createPage({
+			path: node.url,
+			component: postTemplate,
+			context: {
+				// Data passed to context is available
+				// in page queries as GraphQL variables.
+				slug: node.slug,
+			},
+		})
+	})
 
-        createPage({
-            path: node.url,
-            component: postTemplate,
-            context: {
-                // Data passed to context is available
-                // in page queries as GraphQL variables.
-                slug: node.slug,
-            },
-        })
-    })
-
-    // Create pagination
-    paginate({
-        createPage,
-        items: posts,
-        itemsPerPage: postsPerPage,
-        component: indexTemplate,
-        pathPrefix: ({ pageNumber }) => {
-            if (pageNumber === 0) {
-                return `/`
-            } else {
-                return `/page`
-            }
-        },
-    })
+	// Create pagination
+	paginate({
+		createPage,
+		items: posts,
+		itemsPerPage: postsPerPage,
+		component: indexTemplate,
+		pathPrefix: ({ pageNumber }) => {
+			if (pageNumber === 0) {
+				return `/`
+			} else {
+				return `/page`
+			}
+		},
+	})
 }
