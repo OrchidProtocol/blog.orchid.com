a74e787e536582b0c5d1bf8d0b307d4a19cf0d60
calebbabin <object.caleb@gmail.com>
2019-12-12 10:41:18 -0600

    remove purgecss plugin to fix blockquote styling in blog content

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,13 +66,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
+    /*{
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
         purgeOnly: ['/all.css'], // applies purging only on the bulma css file
       },
-    }, // must be after other CSS plugins
+    }, // must be after other CSS plugins*/
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
