0c53fb0c04b3c110ee0e23f5032b1e34dc69f8b5
calebbabin <object.caleb@gmail.com>
2019-12-30 11:36:06 -0600

    add a default description to the index page of the blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,14 @@ import config from '../utils/config';
 export default () => (<>
 		<Helmet>
 			<title>Blog - {config.title}</title>
-			
+			<meta name="description" content="Your VPN should be secure, which is why Orchid is building with open source tools for custom VPN configurations and privacy services." />
+
 			<meta property="og:image" content={config.feature_image} />
 			<meta property="og:image:width" content="1688"/>
 			<meta property="og:image:height" content="950"/>
 			<meta name="twitter:image" content={config.feature_image} />
 			<meta name="twitter:card" content="summary_large_image" />
+			
 
 			<script type="application/ld+json">{`
 				{
