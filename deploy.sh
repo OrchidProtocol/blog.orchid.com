3c6340af6409f95a7d47236385b419c1dc54153f
Nathan Handler <nhandler@orchid.com>
2020-01-27 15:04:46 -0800

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -x
+# deploy.sh: -*- Shell-script -*-  Deploy this website to AWS S3 via CodeCommit.
+#
+#  Copyright (c) 2019 Brian Fox
+#  Author: Brian Fox (bfox@brianjfox.com)
+#  Birthdate: Mon Mar  4 14:03:59 2019.
+
+built_files=dist/static
+
+function usage () {
+    local exit_status="$1"
+    echo "Usage:  deploy [opuslogica | staging [{ja,ko,zh}] | production [{ja,ko,zh}]"
+    echo "Deploys the built website to the specified destination."
+    exit $exit_status
+}
+
+if [ "$1" == "" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then usage 3; fi
+
+function copy-push () {
+    local gitdest="$1"
+    local tempdir=$(mktemp -d)
+    rsync -av --progress . "$tempdir" --exclude .git --exclude .gitignore
+    pushd "$tempdir"
+    #git init
+    #git add .
+    #git commit -m "deployment"
+    #git remote add aws "$gitdest"
+    #git push --force aws master
+    bucket=$(echo "$gitdest" | sed 's/.*\///')
+    aws s3 sync --acl public-read --delete . "s3://$bucket"
+    distribution=$(aws cloudfront list-distributions | jq -r --arg bucket "$bucket" '.DistributionList.Items[] | select(.Status=="Deployed") | select(.Aliases.Items[] | contains($bucket)) | .Id')
+    aws cloudfront create-invalidation --distribution-id "$distribution" --paths "/*"
+    popd
+    rm -rf "$tempdir"
+}
+
+yarn
+
+Build the static site...
+if [ "${#2}" == "2" ]; then
+    yarn run build:static:${2}
+else
+    yarn run build:static
+fi
+
+if [ "$1" == "staging" ] && [ "${#2}" == "2" ]; then
+    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/blog.${2}.orchid.dev"
+elif [ "$1" == "staging" ]; then
+    copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/blog.orchid.dev'
+elif [ "$1" == "production" ] && [ "${#2}" == "2" ]; then
+    copy-push "ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/blog.${2}.orchid.com"
+elif [ "$1" == "production" ]; then
+    copy-push 'ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/blog.orchid.com'
+fi
