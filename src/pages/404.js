fa94b274d7c3673935c118e4df25c4864e6bc71a
calebbabin <object.caleb@gmail.com>
2020-01-28 14:09:12 -0600

    update 404 page text

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
   <Layout>
     <div css="text-align:center">
       <h1>NOT FOUND - 404</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>This page does not exist</p>
     </div>
   </Layout>
 )
